0d00022a686437c57e9208acb3d801fb7440bd1c
danielaconceicao <daniela.3597@gmail.com>
2024-09-30 17:20:01 +0200

    finishing the project

--- main.js
+++ main.js
@@ -3,7 +3,23 @@ let kilometer = prompt('Inserisca il chilometri che desideri percorrere');
 let passengerAge = prompt("inserisca l'età del passeggero");
 
 //il prezzo del biglietto è definito in base ai km (0.21 € al km)
-let ticketPrice = 0.21;
+let pricePerKilometre = 0.21;
 
-let pricePerKilometre = kilometer * ticketPrice;
-console.log(pricePerKilometre);
+let ticketPrice = Number(kilometer * pricePerKilometre);
+let message;
+let discount;
+
+if (passengerAge < 18) {
+    discount = (20 / 100) * ticketPrice;
+    message = `the total discounted price is ${discount.toFixed(2)}`
+}else if (passengerAge > 18 && passengerAge <= 64) {
+    message = `the total price of the trip is ${ticketPrice.toFixed(2)}`
+}else{
+    discount = (40 / 100) * ticketPrice;
+    message = `the total discounted price is ${discount.toFixed(2)}`
+}
+
+
+console.log(message);
+discount.toLocaleString('it-IT', {style: 'currency', currency: 'EUR'}) 
+
